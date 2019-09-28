a140bed656c9afa8c085bb69917e420dee618d80
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-09-28 01:27:52 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -5,7 +5,7 @@ end
 
 def deal_card
   # code #deal_card here
-  card = rand(1..11)
+  rand(1..11)
 end
 
 def display_card_total(card_total)
@@ -20,7 +20,7 @@ end
 
 def get_user_input
   # code #get_user_input here
-  user_input = gets.chomp
+  gets.chomp
 end
 
 def end_game(card_total)
@@ -28,25 +28,27 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(deal_card, deal_card)
+def initial_round
   # code #initial_round here
-  sum = deal_card
-  sum += deal_card
+  sum = deal_card + deal_card
   display_card_total(sum)
+  return sum
 end
 
 def hit?(card_total)
   # code hit? here
   prompt_user
   user_input = get_user_input
-  if get_user_input == 's'
-  elsif get_user_input =='h'
-    deal_card
+  if user_input == 's'
+    return card_total
+  elsif user_input =='h'
+    card_total += deal_card
+    return card_total
   else
     invalid_command
-    prompt_user
+    hit?(card_total)
   end
-  card_total
+  return card_total
 end
 
 def invalid_command
@@ -60,5 +62,12 @@ end
 
 def runner
   # code runner here
+  welcome
+  card_total = initial_round
+  while card_total <= 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
